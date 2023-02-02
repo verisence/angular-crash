3d156701f7b953a9eca81ced57a2c0a4dcef0078
Phil Kipkemboi <kipkemboiphil@gmail.com>
2023-02-02 21:32:54 +0300

    Add task service

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { faDeleteLeft, faDemocrat } from '@fortawesome/free-solid-svg-icons';
+import { faDhl } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -8,5 +10,6 @@ import { Task } from 'src/app/Task';
 })
 export class TaskItemComponent {
   @Input() task!: Task
+  faDelete = faDeleteLeft
 
 }
