b4b779bea948a357d7edbcb95bf83fc6420554fd
Phil Kipkemboi <kipkemboiphil@gmail.com>
2023-02-08 03:21:16 +0300

    toggle the color and text of the button in the header to show or hide the new task form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from "../../services/ui.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-header',
@@ -7,13 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit{
   title: string = 'Task Tracker';
-  constructor(){}
+  showAddTask!  : boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService:UiService){
+    this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value)
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log('Toggle Add Task');
-
+    this.uiService.toggleAddTask();
   }
 
 }
